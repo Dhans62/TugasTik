f34c7ac13bea84aa8d5a31d357f7cfad17fdd32b
dhan <142086577+Dhans62@users.noreply.github.com>
2025-07-27 14:51:30 +0700

    aritmatika.h

---
+++ penjumlahan.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+int A,B;
+
+printf ("masukkan angka A: ");
+scanf ("%d", &A);
+printf ("masukkan angka B: ");
+scanf ("%d", &B);
+printf ("hasil dari A + B= %d\n", A+B);
+printf ("hasil dari A - B= %d\n", A-B);
+printf ("hasil dari A x B= %d\n", A*B);
+printf ("hasil dari A ÷ B= %d\n", A/B);
+return 0;
+}
